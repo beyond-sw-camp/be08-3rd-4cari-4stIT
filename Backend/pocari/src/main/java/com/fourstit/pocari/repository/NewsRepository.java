05f3f645206e38444f0b3760ee5f4564f688354b
mmvne <45449480+mmvne@users.noreply.github.com>
2024-08-16 19:02:15 +0900

    Delete Backend/pocari directory

--- Backend/pocari/src/main/java/com/fourstit/pocari/repository/NewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fourstit.pocari.repository;
-
-import com.fourstit.pocari.entity.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Long> {
-}
--- Backend/pocari/src/main/java/com/fourstit/pocari/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fourstit.pocari.repository;
-
-import com.fourstit.pocari.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
