96350cc3e7f827c4a2872570014c01c448970c0c
uzz99 <yj.lee991228@gmail.com>
2024-08-17 17:25:20 +0900

    [frontend] feat: add Pinia store

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios';
-// import store from './store';
+import store from './store/index';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -14,7 +14,7 @@ import './assets/HFcss.css';
 const app = createApp(App);
 
 app.use(router);
-// app.use(store);
+app.use(store);
 app.config.globalProperties.$axios = axios;
 app.component('HeaderIT', HeaderIT);
 app.mount('#app');
