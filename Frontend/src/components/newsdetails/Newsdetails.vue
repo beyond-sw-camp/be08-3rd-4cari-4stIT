47eaa0e74337dee4f0658402f35ba9ef7160e6ee
uzz99 <yj.lee991228@gmail.com>
2024-08-18 21:58:01 +0900

    [frontend] feat: add bookmark add/remove button

--- Frontend/src/components/newsdetails/Newsdetails.vue
+++ Frontend/src/components/newsdetails/Newsdetails.vue
@@ -12,7 +12,9 @@
         </div>
         <div class="right">
           <span class="views">조회수 {{ news.views }}</span>
-          <button @click="createBookmark" class="bookmark">북마크에 추가</button>
+            <button @click="statusBookmark" class="bookmark">
+              {{ isBookmarked ? '북마크 삭제' : '북마크에 추가' }}
+          </button>
         </div>
       </div>
 
@@ -30,25 +32,21 @@
       </div>
       <br>
 
-    <div class="navigation-arrows">
-        <div class="go-back">
-          <router-link to="/main" class="go-back-link">
-            <img src="@/assets/go_back.png" class="goback_img" alt="뒤로 가기" width="20px"/>
-          </router-link>
+    <!-- <div class="navigation-arrows">
+        <div v-if="goBack"class="go-back">
+            <img src="@/assets/go_back.png" @click="backTopage" class="goback_img" alt="뒤로 가기" width="20px"/>
         </div>
         <div class="go-forward">
-          <router-link to="/main" class="go-forward-link">
             <img src="@/assets/go.png" class="go_img" alt="앞으로 가기" width="20px"/>
-          </router-link>
         </div>
-      </div>
+      </div> 
+    -->
       
       <div class="back-to-list">
         <router-link :to="backToList" class="back-button">목록으로 돌아가기</router-link>
       </div>
 
     </div>
-
 
 </template>
 
@@ -57,31 +55,69 @@
 import axios from 'axios';
 import '@/components/newsdetails/Newsdetails.css'
 import { useRouter, useRoute } from 'vue-router';
+import { computed, ref, onMounted } from 'vue';
 import { useUserStore } from '@/store/user';
-import { computed } from 'vue';
+// import { useNavigatorStore } from '@/store/navigator';
+import { useBookmarkStore } from '@/store/bookmark';
 
 export default {
   name: "DetailNews",
   setup(){
     const userStore = useUserStore();
+    // const naviStore = useNavigatorStore();
+    const bookmarkStore = useBookmarkStore();
+
     const router = useRouter();
     const route = useRoute();
 
-    const createBookmark = async () => {
+    // const newsIdx = ref(-1);
+    const curNewsNo = Number(route.params.newsNo);
+
+    const bookmarkList = computed(() => bookmarkStore.bookmarkList);
+    const isBookmarked = computed(() => bookmarkList.value.some(news => news.newsId === curNewsNo));
+
+    const fetchBookmarkList = async () => {
+      if (userStore.isLogIn) {
+        try {
+          const response = await axios.get(`http://localhost:8080/api/bookmark/${userStore.user.id}`);
+          bookmarkStore.setBookmarkList(response.data);
+        } catch (error) {
+          console.error("북마크 로드 실패:", error);
+        }
+      }
+    };
+
+    const statusBookmark = async () => {
       if (!userStore.isLogIn) {
         alert('로그인이 필요합니다.');
         router.push('/login');
         
         return;
-      }
-
+      };
+      // console.log("isBookmarked: ", isBookmarked.value);
       try {
+        if(isBookmarked.value){
+        
+          await axios.delete('http://localhost:8080/api/deletebookmark', {
+            params: {
+              userId: userStore.user.id,
+              newsId: curNewsNo
+            }
+        });
+        alert("북마크 삭제 완료");
+        bookmarkStore.deleteBookmark(curNewsNo);
+
+        
+        }else{
+
         await axios.post('http://localhost:8080/api/createbookmark', {
           userId: userStore.user.id,
-          newsId: route.params.newsNo
+          newsId: curNewsNo     
         });
-        
         alert("북마크 추가 완료");
+        bookmarkStore.addBookmark(curNewsNo);
+
+        }
       } catch (error) {
         if (error.response && error.response.status === 409) {
         alert('이미 추가된 뉴스입니다.');
@@ -91,13 +127,20 @@ export default {
       }
     };
 
+    onMounted(() => {
+      fetchBookmarkList();
+    });
+
     const backToList = computed(() => {
       return userStore.isLogIn ? '/bookmark' : '/';
     });
 
     return {
-      createBookmark,
-      backToList
+      isBookmarked,
+      statusBookmark,
+      backToList,
+      bookmarkList,
+      // newsIdx,
     };
   },
 
@@ -116,11 +159,13 @@ export default {
       try {
         const newsNo = this.$route.params.newsNo;
         const response = await axios.get(`http://localhost:8080/api/detail/${newsNo}`);
+        console.log('News data:', response.data);
         this.news = response.data;
       } catch (error) {
         console.error("존재하지 않는 뉴스입니다. : ", error);
       }
     },
+
     getNewsById(id) {
           NewsService.getNewsById(id).then(response => {
             this.newsList = [response.data];
