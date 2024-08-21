52a7130316e4e1e098c8285cf7876d6b34ea20ea
mmvne <kingtjrals@naver.com>
2024-08-21 12:49:07 +0900

    뉴스 상세 카테고리 추가

--- Backend/src/main/java/com/fourstit/pocari/repository/BookmarkRepository.java
+++ Backend/src/main/java/com/fourstit/pocari/repository/BookmarkRepository.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 public interface BookmarkRepository extends CrudRepository<Bookmark, Long> {
 
+    // 북마크 된 뉴스 조회 ( with 카테고리 )
     @Query("SELECT new com.fourstit.pocari.dto.BookmarkDto(b.bookmarkId, b.newsId, n.title, n.summary, c.name, n.image, b.createdDate) " +
             "FROM Bookmark b " +
             "JOIN News n ON b.newsId = n.newsId " +
